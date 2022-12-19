fd304f91fe7f00bc20985e1c3fd91106b9cc86a9
kwokieee <bryankwokyf@gmail.com>
2022-12-19 16:41:33 +0800

    Add Carousel component

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, useState } from "react";
+import { Icon } from '@iconify/react';
+
+interface CarouselProps {
+  slides: Array<ReactNode>;
+}
+
+function Carousel({ slides = [] }: CarouselProps) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const length = slides.length;
+  const nextSlide = () => setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1);
+  // const nextSlide = () => console.log("clicked")
+  const prevSlide = () => setCurrentSlide(currentSlide === 0 ? length - 1 : currentSlide - 1);
+
+  return (
+    <div className="relative w-full h-full">
+      <Icon icon="material-symbols:arrow-circle-right-rounded" onClick={nextSlide} className="absolute right-0 top-[50%] z-20" width={50} height={50} />
+      <Icon icon="material-symbols:arrow-circle-left-rounded" onClick={prevSlide} className="absolute left-0 top-[50%] z-20" width={50} height={50} />
+      {slides.map((slide, index) => {
+        return (
+          <div key={index} className={index === currentSlide ? 'w-full h-full' : 'hidden w-full h-full'}>
+            {index === currentSlide && slide}
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Carousel
