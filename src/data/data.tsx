1488fe012d9a36578d63d519530dbf160e5427ad
kwokieee <bryankwokyf@gmail.com>
2022-12-19 12:58:21 +0800

    Refactor data

---
+++ src/data/data.tsx
@@ -0,0 +1,25 @@
+/**
+ * Section definition
+ */
+export const SectionId = {
+    Hero: 'hero',
+    About: 'about',
+    Skills: 'skills',
+    Work: 'work',
+    Projects: 'projects',
+    Contact: 'contact',
+};
+
+/**
+ * Hero data
+ */
+export const heroData = {
+    imagePath: '/images/'
+}
+
+/**
+ * External links
+ */
+export const cityosVideoUrl = 'https://www.youtube.com/watch?v=J7uF4DWtZ-U&ab_channel=STEngineering'
+  
+export type SectionId = typeof SectionId[keyof typeof SectionId];
