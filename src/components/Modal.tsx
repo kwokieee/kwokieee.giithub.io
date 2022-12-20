855044d88b73dede380afe7d6604acdd1110a626
kwokieee <bryankwokyf@gmail.com>
2022-12-20 12:45:05 +0800

    Adjust padding

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,7 @@ interface ModalProps {
 
 function Modal({handleClose = () => {}, show = false, children}: ModalProps) {
   return show
-    ? <div className="fixed left-0 top-0 w-screen h-screen max-w-screen max-h-screen flex justify-center items-center bg-slate-500/75" onClick={handleClose}>
+    ? <div className="fixed left-0 top-0 py-8 w-screen h-screen max-w-screen max-h-screen flex justify-center items-center bg-slate-500/75" onClick={handleClose}>
         <div className="relative w-10/12 h-10/12 max-h-full shadow-lg bg-white z-10 rounded-md overflow-y-auto" onClick={e => e.stopPropagation()}>
           <div className="absolute right-2 top-2 flex justify-end">
             <Icon icon="gridicons:cross" className="cursor-pointer w-8 h-8" onClick={handleClose} />
