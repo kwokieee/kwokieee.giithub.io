0db390bdbb6433b3c078026d2cbc014aed3253a1
kwokieee <bryankwokyf@gmail.com>
2022-12-21 13:27:53 +0800

    Update card sizing

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,8 +15,8 @@ function Card({ header, body, imagePath = "", imageDescription = "", onClick = (
 
   return (
     <>
-      <div onClick={() => setIsModalShown(true)} className="bg-gray-200 rounded-md overflow-hidden shadow-lg w-56 sm:w-full max-w-4xl h-auto flex flex-col cursor-pointer">
-        {imagePath && imageDescription && <img src={imagePath} alt={imageDescription} className="h-auto object-cover bg-white" />}
+      <div onClick={() => setIsModalShown(true)} className="bg-gray-200 rounded-md overflow-hidden shadow-lg w-56 sm:w-full max-w-3xl h-auto flex flex-col cursor-pointer">
+        {imagePath && imageDescription && <img src={imagePath} alt={imageDescription} className="h-auto max-h-96 object-cover bg-white" />}
         <h1 className="text-xl font-bold text-left ml-4 p-2">{header}</h1>
         <p className="text-sm text-left ml-4 px-2 pb-2">{body}</p>
       </div>
