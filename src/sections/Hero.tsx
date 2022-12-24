884f8a48d8cd205ceb1e01230c120d27a0187ade
kwokieee <bryankwokyf@gmail.com>
2022-12-24 18:10:18 +0800

    Improve Hero section responsiveness

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,11 +4,11 @@ import { SectionId } from "../data/data"
 function Hero() {
   return (
     <Section noPadding sectionId={SectionId.Hero}>
-      <div className="sm:h-screen lg:h-auto flex flex-col lg:flex-row">
+      <div className="h-screen lg:h-auto flex flex-col lg:flex-row">
         <div className="lg:max-w-[1000px]">
           <img src="/images/hero-photo.png" className="object-cover" />
         </div>
-        <div className="bg-[#393434] p-6 flex flex-col justify-center items-center sm:h-full lg:h-auto sm:min-w-[500px] w-full">
+        <div className="bg-[#393434] p-6 flex flex-col justify-center items-center h-full lg:h-auto w-full">
           <p className="text-[#eeeeee] text-3xl sm:text-4xl lg:text-6xl font-extrabold">Hi, I'm Bryan.</p>
           <div className="bg-[#eeeeee] h-0.5 w-5/6 max-w-xl my-1 sm:my-2 lg:my-6 rounded-sm" />
           <p className="text-[#eeeeee] sm:text-2xl lg:text-3xl xl:text-4xl">I learn, I design,</p>
