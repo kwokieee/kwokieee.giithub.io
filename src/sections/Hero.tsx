09d7be5275758ff29273e714fe35ca86cee848d1
kwokieee <bryankwokyf@gmail.com>
2022-12-25 14:37:53 +0800

    Modify mobile layout breakpoint

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,15 +4,15 @@ import { SectionId } from "../data/data"
 function Hero() {
   return (
     <Section noPadding sectionId={SectionId.Hero}>
-      <div className="h-screen lg:h-auto flex flex-col lg:flex-row">
-        <div className="lg:max-w-[1000px]">
+      <div className="h-screen md:h-auto flex flex-col md:flex-row">
+        <div className="md:max-w-[1000px]">
           <img src="/images/hero-photo.png" className="object-cover" />
         </div>
-        <div className="bg-[#515151] p-6 flex flex-col justify-center items-center h-full lg:h-auto w-full">
-          <p className="text-[#eeeeee] text-4xl lg:text-6xl font-extrabold">Hi, I'm Bryan.</p>
-          <div className="bg-[#eeeeee] h-0.5 w-5/6 max-w-xl my-3 sm:my-2 lg:my-6 rounded-sm" />
-          <p className="text-[#eeeeee] sm:text-2xl lg:text-3xl xl:text-4xl">I learn, I design,</p>
-          <p className="text-[#eeeeee] sm:text-2xl lg:text-3xl xl:text-4xl">and I build things.</p>
+        <div className="bg-[#515151] p-6 flex flex-col justify-center items-center h-full md:h-auto w-full">
+          <p className="text-[#eeeeee] text-4xl md:text-4xl lg:text-6xl font-extrabold">Hi, I'm Bryan.</p>
+          <div className="bg-[#eeeeee] h-0.5 w-5/6 max-w-xl my-3 sm:my-2 md:my-6 rounded-sm" />
+          <p className="text-[#eeeeee] sm:text-2xl xl:text-4xl">I learn, I design,</p>
+          <p className="text-[#eeeeee] sm:text-2xl xl:text-4xl">and I build things.</p>
         </div>
       </div>
     </Section>
