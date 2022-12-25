0fcc83e713fbe85f278e0e2ba690fb4b30aeaa43
kwokieee <bryankwokyf@gmail.com>
2022-12-25 14:40:01 +0800

    Change image sizings

--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -7,11 +7,94 @@ import Rover from "../content/Rover"
 import Climbjios from "../content/Climbjios"
 import Ridgular from "../content/Ridgular"
 import Seatseer from "../content/Seatseer"
+import Carousel from "../components/Carousel"
+import Modal from "../components/Modal"
+import { useState } from "react"
 
 function Work() {
+  const [isSTEngModalOpen, setSTEngModalOpen] = useState(false);
+  const [isClimbjiosModalOpen, setClimbjiosModalOpen] = useState(false);
+  const [isTAModalOpen, setTAModalOpen] = useState(false);
+  const [isRoverModalOpen, setRoverModalOpen] = useState(false);
+  const [isRidgularModalOpen, setRidgularModalOpen] = useState(false);
+  const [isSeatseerModalOpen, setSeatseerModalOpen] = useState(false);
+
   return (
-    <Section sectionId={SectionId.Work}>
+    <Section noPadding sectionId={SectionId.Work}>
       <div className="flex flex-col justify-center items-center">
+        <div className="flex items-center w-full mb-20 px-8 sm:px-16">
+          <span className="bg-[#393434] h-1 w-4 rounded-sm grow" />
+          <h1 className="text-4xl font-extrabold px-2">My Work</h1>
+          <span className="bg-[#393434] h-1 w-4 rounded-sm grow" />
+        </div>
+        <Carousel
+          slides={[
+            <img src="/images/st-engineering.png" className="object-scale-down" />,
+            <img src="/images/climbjios.png" className="object-scale-down" />,
+            <img src="/images/source-academy.png" className="object-scale-down" />,
+            <img src="/images/rover-team.jpg" className="object-scale-down" />,
+            <img src="/images/ridgular.png" className="object-scale-down" />,
+            <img src="/images/seatseer.png" className="object-scale-down" />,
+          ]}
+          largeCaptions={[
+            {
+              caption: "ST Engineering",
+              subCaption: "My work on cityOS, a platform for facility management.",
+              buttonText: "Find out more",
+              onClick: () => setSTEngModalOpen(true)
+            },
+            {
+              caption: "ClimbJios",
+              subCaption: "An open source progressive web app for Singapore's climbing community.",
+              buttonText: "Find out more",
+              onClick: () => setClimbjiosModalOpen(true)
+            },
+            {
+              caption: "Undergraduate Teaching Assistant",
+              subCaption: "My experience teaching the CS1101S Programming Methodology module.",
+              buttonText: "Find out more",
+              onClick: () => setTAModalOpen(true)
+            },
+            {
+              caption: "NUS Rover Team",
+              subCaption: "My time as a member and a project manager of the NUS Rover Team.",
+              buttonText: "Find out more",
+              onClick: () => setRoverModalOpen(true)
+            },
+            {
+              caption: "Ridgular",
+              subCaption: "A progressive web app for ride hailing for those who have regular ride booking schedules.",
+              buttonText: "Find out more",
+              onClick: () => setRidgularModalOpen(true)
+            },
+            {
+              caption: "SeatSeer",
+              subCaption: "A mobile app for checking seat availability around NUS.",
+              buttonText: "Find out more",
+              onClick: () => setSeatseerModalOpen(true)
+            },
+          ]}
+        />
+        <Modal handleClose={() => setSTEngModalOpen(false)} show={isSTEngModalOpen}>
+          <Cityos />
+        </Modal>
+        <Modal handleClose={() => setClimbjiosModalOpen(false)} show={isClimbjiosModalOpen}>
+          <Climbjios />
+        </Modal>
+        <Modal handleClose={() => setTAModalOpen(false)} show={isTAModalOpen}>
+          <TA />
+        </Modal>
+        <Modal handleClose={() => setRoverModalOpen(false)} show={isRoverModalOpen}>
+          <Rover />
+        </Modal>
+        <Modal handleClose={() => setRidgularModalOpen(false)} show={isRidgularModalOpen}>
+          <Ridgular />
+        </Modal>
+        <Modal handleClose={() => setSeatseerModalOpen(false)} show={isSeatseerModalOpen}>
+          <Seatseer />
+        </Modal>
+      </div>
+      {/* <div className="flex flex-col justify-center items-center">
         <div className="flex items-center w-full mb-16">
           <span className="bg-[#393434] h-1 w-4 rounded-sm grow" />
           <h1 className="text-4xl font-extrabold px-2">My Work</h1>
@@ -37,7 +120,7 @@ function Work() {
             <Seatseer />
           </Card>
         </div>
-      </div>
+      </div> */}
     </Section>
   )
 }
