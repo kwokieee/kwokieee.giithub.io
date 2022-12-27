d14c9060830ced531ff84b46e15fa63521109595
kwokieee <bryankwokyf@gmail.com>
2022-12-27 13:13:00 +0800

    Close nav bar upon clicking nav link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,14 +6,16 @@ import { sections } from "../data/data";
 interface NavLinkProps {
   navLinkId: string;
   scrollToId: string;
+  closeNavBar?: VoidFunction;
   children: ReactNode
 }
 
-function NavLink({ navLinkId, scrollToId, children }: NavLinkProps) {
+function NavLink({ navLinkId, scrollToId, closeNavBar = () => {}, children }: NavLinkProps) {
   const [activeNavLinkId, setActiveNavLinkId] = useState("");
   const handleClick = () => {
     setActiveNavLinkId(activeNavLinkId);
     document.getElementById(scrollToId)?.scrollIntoView({ behavior: "smooth" });
+    closeNavBar();
   }
 
   return (
@@ -25,8 +27,12 @@ function NavLink({ navLinkId, scrollToId, children }: NavLinkProps) {
 
 function NavBar() {
   const [isNavBarExpanded, setIsNavBarExpanded] = useState(false);
-  const toggleNavBarExpanded = () => {
-    setIsNavBarExpanded(!isNavBarExpanded);
+  const openNavBar = () => {
+    setIsNavBarExpanded(true);
+  }
+
+  const closeNavBar = () => {
+    setIsNavBarExpanded(false);
   }
   
   return (
@@ -41,13 +47,13 @@ function NavBar() {
       </div>
       {
         isNavBarExpanded
-          ? <Icon icon="gridicons:cross" color="#eeeeee" className="md:hidden absolute right-6 w-6 h-6 cursor-pointer" onClick={toggleNavBarExpanded} />
-          : <Icon icon="material-symbols:menu-rounded" color="#eeeeee" className="md:hidden absolute right-6 w-6 h-6 cursor-pointer" onClick={toggleNavBarExpanded} />
+          ? <Icon icon="gridicons:cross" color="#eeeeee" className="md:hidden absolute right-6 w-6 h-6 cursor-pointer" onClick={closeNavBar} />
+          : <Icon icon="material-symbols:menu-rounded" color="#eeeeee" className="md:hidden absolute right-6 w-6 h-6 cursor-pointer" onClick={openNavBar} />
       }
       <div className={"bg-[#393434] absolute top-14 left-0 w-full flex flex-col md:relative md:top-0 md:right-0 md:w-auto md:block ml-auto " + (isNavBarExpanded ? "" : "hidden")}>
       {
         sections.map((section, index) =>
-          section.isInNavBar && <NavLink key={index} navLinkId={section.sectionTitle} scrollToId={section.sectionId}>{section.sectionTitle}</NavLink>)
+          section.isInNavBar && <NavLink key={index} navLinkId={section.sectionTitle} scrollToId={section.sectionId} closeNavBar={closeNavBar}>{section.sectionTitle}</NavLink>)
       }
       </div>
     </nav>
